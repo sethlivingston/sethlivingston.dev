299fac528978fefaea7b4327eb0db4f1f4651aaf
Seth Livingston <seth@moov.io>
2022-10-19 15:30:26 -0500

    More font tweaking

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Seth Livingston";
-export const SITE_DESCRIPTION = "My personal laboratory";
+export const SITE_DESCRIPTION = "My laboratory in the open";
